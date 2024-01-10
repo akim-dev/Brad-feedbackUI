81d5f40f6862ad94b37c568ab0171d0144f65e06
musta.akim <musta.akim@gmail.com>
2024-01-10 11:21:47 +0700

    penambahan button dan stats

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,11 +1,25 @@
-import React from "react";
 import Card from "../shared/Card";
 import { useState } from "react";
+import Button from "../shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    // check if text is not empty for validation
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage("Text must be at least 10 characters");
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
+
     setText(e.target.value);
   };
   return (
@@ -19,8 +33,11 @@ function FeedbackForm() {
             placeholder="'Write review"
             onChange={handleTextChange}
           />
-          <button type="submit">Send</button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
